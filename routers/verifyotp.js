dbae38a6cd948e287a0382e3daa9efdbc784ed17
Techteam-kr <103136184+Techteam-kr@users.noreply.github.com>
2022-07-10 22:58:50 +0530

    changed verify otp

--- routers/verifyotp.js
+++ routers/verifyotp.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Otp = require('../models/smsschema')
+const Users = require('../models/formSchema')
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
@@ -11,21 +12,49 @@ mongoose.connect("mongodb://localhost:27017/applicants")
 router.post('/', async(req,res) => {
      let isOTPValidated = "false";
      
-    OTP = Otp.findOne({ mobileno: req.body.mobno}, function (err, docs) {
+     OTP = Otp.findOne({ mobileno: req.body.mobno}, function (err, docs) {
         if (err){
             console.log(err);
         }
         else{
-            console.log(docs.toObject().otp)
-            savedOTP = docs.toObject().otp
-            recievedOTP = req.body.otp
+
+            if(docs === null){
+                res.send("Mobile Number not Found")
+            } else {
+
+                console.log(docs.toObject().otp)
+                savedOTP = docs.toObject().otp
+                recievedOTP = req.body.otp
                 if(savedOTP == recievedOTP){
                     isOTPValidated = "true";
-                    res.send(isOTPValidated)
+
+                    Users.find({mobileNumber: req.body.mobno}, function (err, docs){
+
+                        if (err){
+                            console.log(err);
+                            res.status(450).send('Error')
+                            return
+                        }
+                        else {
+                            console.log(docs)
+                            isValidated = {
+                                status : "true",
+                                name : docs[0].fullName
+                            }
+                            res.status(200).send(isValidated)
+                            return
+                        }
+                    });
+
+                    //res.send(isOTPValidated)
                 } else {
                     isOTPValidated = "false";
                     res.send(isOTPValidated)
-                } 
+                }
+
+            }
+
+             
         }
     });
 })
